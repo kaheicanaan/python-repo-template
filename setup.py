70d5343b3af0b5c875f4c560a1ba8253cdabb3d1
NG KA HEI <alankhng@atal.com.hk>
2020-05-29 11:52:26 +0800

    Update setup.py: using package_dir field.

--- setup.py
+++ setup.py
@@ -20,19 +20,19 @@ if __name__ == '__main__':
         where=package_root,
         exclude=['*.DS_Store']
     )
-    package_paths = [f'{package_root}.{path}' for path in package_paths]
-    print(package_paths)
+    print('package_paths:', package_paths)
 
     setup(
-        name=packageName,
+        name='sample_repo',
         version='0.0.1b0',  # versioning standard: https://www.python.org/dev/peps/pep-0440/
         description='Sample repository structure',
         author='Ka Hei Ng, Alan',
         author_email='kaheicanaan@gmail.com',
         classifiers=['Programming Language :: Python :: 3.8'],
         url='https://github.com/kaheicanaan/python-repo-template',
+        package_dir={'': 'src'},
         packages=package_paths,
-        keywords=['gbComp', 'Computation', 'pyComp'],
+        keywords=['demo'],
         install_requires=[
             'pandas==0.25.3'
         ],
